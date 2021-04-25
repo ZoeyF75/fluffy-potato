96be46d3a6eac96d064c4d9ad58a157697dcec4d
ZoeyF75 <zf.zoodle@gmail.com>
2021-04-25 16:53:43 -0700

    border is uniform

--- debugII.js
+++ debugII.js
@@ -1,7 +1,7 @@
 const printInFrame = function(list) {
   list = list.split(' ');
   const longest = longestStr(list);
-  const border = repeat('* ', longest);
+  const border = repeat('*', longest+4);
 
   console.log(border);
   for (const word of list) {
